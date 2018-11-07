914e52288b73a35e0f6a0e2fbc7ce340cb67f79b
Ahmed Hajat <ahmedhajat@gmail.com>
2018-11-07 21:15:34 +0200

    design final

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 $(function () {
 
   var isMobile;
@@ -88,14 +89,6 @@ $(function () {
     $('.link-wrap').toggleClass('visible');
   });
 
- /*  $('.blog-wrap').hover(function () {
-    $('.blog-wrap').not(this).addClass('fade');
-    $(this).addClass("hover");
-  }, function () {
-    $(this).removeClass("hover");
-    $('.blog-wrap').removeClass('fade');
-  }); */
-
   posFilterBar($('.filter').first());
 
   $('.filter').click(function () {
@@ -146,21 +139,6 @@ $(function () {
   }
 
   setTimeout(function () { onScrollInit($('.waypoint')) }, 10);
-
-/*   // CONTACT FORM
-  $('#contact-form').submit(function (e) {
-    e.preventDefault();
-
-    $.ajax({
-      url: "https://formspree.io/ahmedhajat@gmail.com",
-      method: "POST",
-      data: { message: $('form').serialize() },
-      dataType: "json"
-    }).done(function (response) {
-      $('#success').addClass('expand');
-      $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-    });
-  }); */
 
   $('#close').click(function () {
     $('#success').removeClass('expand');
